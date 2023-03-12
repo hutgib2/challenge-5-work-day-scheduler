e1c2dcdc6185658d8609ccee5b70c558258f0eb3
Hugo Smith <hugowsmith1301@gmail.com>
2023-03-12 15:57:51 +1100

    Add past present future

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function(){
+
+  var now = dayjs();
+  console.log(now.get('hour'));
+
+  $('#currentDay').html(now.format('dddd, MMMM DD YYYY'));
+
+
+  
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -14,6 +23,22 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  for (let i = 9; i < 18; i++) {
+    if (i < now.get('hour')) {
+      $('#hour-' + i).addClass("past");
+    }
+    else if (i == now.get('hour')) {
+      $('#hour-' + i).addClass("present");
+    }
+    else if (i > now.get('hour')) {
+      $('#hour-' + i).addClass("future");
+    }
+    
+  }
+
+  
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
